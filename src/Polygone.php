aaf191d8e0d56bf3471a02f2910ca1d0c6442560
MichelCode <michelseocode.com>
2023-10-10 20:17:50 +0200

    SavePolygonos

--- src/Polygone.php
+++ src/Polygone.php
@@ -2,4 +2,18 @@
 
 namespace Opmvpc\Formes;
 
-class Polygone extends Forme {}
+class Polygone extends Forme
+{
+    private array $points;
+
+    public function __construct(array $points, string $color = "#000000")
+    {
+        parent::__construct($color);
+        $this->points = $points;
+    }
+
+    public function getPoints(): array
+    {
+        return $this->points;
+    }
+}
